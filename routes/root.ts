7f2dee0945f392c835c597ddb7d0f3d55fe3a763
raylu15742cs <raylucs15742@gmail.com>
2023-01-31 11:01:31 -0800

    fix root

--- routes/root.ts
+++ routes/root.ts
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router()
 import path from 'path';
 
-router.get('^/$ | /index(.html)?', (req,res) => {
+router.get('^/$|/index(.html)?', (req,res) => {
     res.sendFile(path.join(__dirname, "..", 'views', 'index.html'))
 })
 
