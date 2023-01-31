32e363ccb43dd4010bb0e11c7060a92e3f64b69f
raylu15742cs <raylucs15742@gmail.com>
2023-01-31 10:56:36 -0800

    update imports

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
 import express from "express";
 const app = express()
-const path = require('path')
+import path from 'path';
 const PORT = process.env.PORT || 3500
 
 app.use("/", express.static(path.join(__dirname, '/public')))
+
+app.use("/", require("./routes/root"))
 
 app.listen(PORT, () =>  console.log(`Server running on port ${PORT}`))
