5d067d42f286b50e8cb40cd9f56781c6f7cabcd1
raylu15742cs <raylucs15742@gmail.com>
2023-01-31 10:44:45 -0800

    initial server

--- server.ts
+++ server.ts
@@ -1,2 +1,5 @@
-const express = require('express')
+import express from "express";
 const app = express()
+const PORT = process.env.PORT || 3500
+
+app.listen(PORT, () =>  console.log(`Server running on port ${PORT}`))
