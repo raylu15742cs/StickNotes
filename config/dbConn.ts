0bea4dd9658ae9b31bacd232fb0238ec90b5a57f
raylu15742cs <raylucs15742@gmail.com>
2023-01-31 16:04:50 -0800

    update strictQuery

--- config/dbConn.ts
+++ config/dbConn.ts
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
+        mongoose.set("strictQuery", false);
         await mongoose.connect(process.env.DATABASE_URI)
     } catch (err) {
         console.log(err)
