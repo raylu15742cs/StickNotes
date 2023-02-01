229df813acad531e4811fee0968f8d1f3979514b
raylu15742cs <raylucs15742@gmail.com>
2023-01-31 16:50:02 -0800

    usercontroller

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -4,7 +4,31 @@ import asyncHandler from "express-async-handler"
 import bcrpt from 'bcrypt'
 
 // @desc Get all users
-
 // @route GET /users
-
 // @access Private
+const getAllUsers = asyncHandler(async (req,res) => {
+
+})
+
+// @desc Create a user
+// @route POST /users
+// @access Private
+const createNewUser = asyncHandler(async (req,res) => {
+    
+})
+
+// @desc Update a user
+// @route PATCH /users
+// @access Private
+const updateUser = asyncHandler(async (req,res) => {
+    
+})
+
+// @desc Delete a user
+// @route DELETE /users
+// @access Private
+const deleteUser = asyncHandler(async (req,res) => {
+    
+})
+
+module.exports = { getAllUsers , createNewUser , updateUser , deleteUser}
