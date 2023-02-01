d7bd033326f7893233b579030ca5d1f149bb0c66
raylu15742cs <raylucs15742@gmail.com>
2023-01-31 16:40:04 -0800

    usercontroller

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,2 +1,10 @@
-const User = require("../models/User")
-const Note = require("../models/Note")
+import User from "../models/User"
+import Note from "../models/Note"
+import asyncHandler from "express-async-handler"
+import bcrpt from 'bcrypt'
+
+// @desc Get all users
+
+// @route GET /users
+
+// @access Private
